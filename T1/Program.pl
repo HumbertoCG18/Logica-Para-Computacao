f8b08e6e4556edc8233fa247333c047235dac46f
Humberto Corrêa Gomes <humberto.gomes002@edu.pucrs.br>
2024-11-11 09:55:43 -0300

    Update Program.pl

--- T1/Program.pl
+++ T1/Program.pl
@@ -1,94 +1,129 @@
-% Base de Conhecimento
+% Declaração dos animais e suas características
 
-% animal(nome,classe,alimntacao,habitat,desenvolvimeno)
+% Animais
+animal(leao).
+animal(tubarao_martelo).
+animal(elefante).
+animal(crocodilo).
+animal(tucano).
+animal(salamandra).
+animal(ornitorrinco).
+animal(gaviao).
+animal(pinguim).
+animal(girafa).
+animal(tartaruga_marinha).
+animal(sapo).
+animal(urso).
+animal(pato).
+animal(raposa).
 
-% Animais e suas características
-animal(leao, mamifero, carnivoro, savana, viviparo).
-animal(tubarao_martelo, peixe, carnivoro, agua_marinha, viviparo).
-animal(elefante, mamifero, herbivoro, savana, viviparo).
-animal(crocodilo, reptil, carnivoro, agua_doce, oviparo).
-animal(tucano, ave, onivoro, floresta, oviparo).
-animal(salamandra, anfibio, carnivoro, floresta, oviparo).
-animal(ornitorrinco, mamifero, onivoro, agua_doce, oviparo).
-animal(gaviao, ave, carnivoro, floresta, oviparo).
-animal(pinguim, ave, carnivoro, agua_marinha, oviparo).
-animal(girafa, mamifero, herbivoro, savana, viviparo).
-animal(tartaruga_marinha, reptil, herbivoro, agua_marinha, oviparo).
-animal(sapo, anfibio, carnivoro, agua_doce, oviparo).
-animal(urso, mamifero, onivoro, floresta, viviparo).
-animal(pato, ave, herbivoro, agua_doce, oviparo).
-animal(raposa, mamifero, carnivoro, floresta, viviparo).
+% Classe dos animais
+classe(mamifero, leao).
+classe(peixe, tubarao_martelo).
+classe(mamifero, elefante).
+classe(reptil, crocodilo).
+classe(ave, tucano).
+classe(anfibio, salamandra).
+classe(mamifero, ornitorrinco).
+classe(ave, gaviao).
+classe(ave, pinguim).
+classe(mamifero, girafa).
+classe(reptil, tartaruga_marinha).
+classe(anfibio, sapo).
+classe(mamifero, urso).
+classe(ave, pato).
+classe(mamifero, raposa).
+
+% Dieta dos animais
+dieta(carnivoro, leao).
+dieta(carnivoro, tubarao_martelo).
+dieta(herbivoro, elefante).
+dieta(carnivoro, crocodilo).
+dieta(onivoro, tucano).
+dieta(carnivoro, salamandra).
+dieta(onivoro, ornitorrinco).
+dieta(carnivoro, gaviao).
+dieta(carnivoro, pinguim).
+dieta(herbivoro, girafa).
+dieta(herbivoro, tartaruga_marinha).
+dieta(carnivoro, sapo).
+dieta(onivoro, urso).
+dieta(herbivoro, pato).
+dieta(carnivoro, raposa).
+
+% Habitat dos animais
+habitat(savana, leao).
+habitat(agua_marinha, tubarao_martelo).
+habitat(savana, elefante).
+habitat(agua_doce, crocodilo).
+habitat(floresta, tucano).
+habitat(floresta, salamandra).
+habitat(agua_doce, ornitorrinco).
+habitat(floresta, gaviao).
+habitat(agua_marinha, pinguim).
+habitat(savana, girafa).
+habitat(agua_marinha, tartaruga_marinha).
+habitat(agua_doce, sapo).
+habitat(floresta, urso).
+habitat(agua_doce, pato).
+habitat(floresta, raposa).
+
+% Reprodução dos animais
+reproducao(viviparo, leao).
+reproducao(viviparo, tubarao_martelo).
+reproducao(viviparo, elefante).
+reproducao(oviparo, crocodilo).
+reproducao(oviparo, tucano).
+reproducao(oviparo, salamandra).
+reproducao(oviparo, ornitorrinco).
+reproducao(oviparo, gaviao).
+reproducao(oviparo, pinguim).
+reproducao(viviparo, girafa).
+reproducao(oviparo, tartaruga_marinha).
+reproducao(oviparo, sapo).
+reproducao(viviparo, urso).
+reproducao(oviparo, pato).
+reproducao(viviparo, raposa).
+
+% Regras
+
+animal_carnivoro(Animal) :- animal(Animal), dieta(carnivoro, Animal).
+animais_vivem_floresta(Animal) :- animal(Animal), habitat(floresta, Animal).
+dietas_animais_savana(Animal, Dieta) :- animal(Animal), habitat(savana, Animal), dieta(Dieta, Animal).
+existe_oviparo_agua_salgada(Existe, Animal) :- habitat(agua_marinha, Animal), reproducao(oviparo, Animal) -> Existe = sim ; Existe = nao.
+existe_mamifero_oviparo(Existe, Animal) :- classe(mamifero, Animal), reproducao(oviparo, Animal) -> Existe = sim ; Existe = nao.
+existe_carnivoro_aguaDoce(Existe, Animal) :- habitat(agua_doce, Animal), dieta(carnivoro, Animal) -> Existe = sim ; Existe = nao.
+animais_terrestres_herbivoros(Animal) :- animal(Animal), dieta(herbivoro, Animal), habitat(Habitat, Animal), Habitat \= agua_marinha, Habitat \= agua_doce.
+voadores_carnivoros(Animal) :- animal(Animal), classe(ave, Animal), dieta(carnivoro, Animal), Animal \= pinguim.
 
 % Consultas
 
 % 1. Qual é o habitat de um animal específico X?
-% habitat(X, Habitat).
-habitat(X, Habitat) :- animal(X, _, _, Habitat, _).
+% habitat(Habitat, X).
 
 % 2. O animal específico Y é um mamífero?
-% eh_mamifero(Y).
-eh_mamifero(Y) :- animal(Y, mamifero, _, _, _).
+% classe(mamifero, Y).
 
 % 3. Quais são todos os animais carnívoros?
-% carnívoros(Animais).
-carnivoros(Animais) :- findall(Animal, animal(Animal, _, carnivoro, _, _), Animais).
+% animal_carnivoro(Animal).
 
-% 4. Quais são todos os animais vivem na floresta?
-% vivem_na_floresta(Animais).
-vivem_na_floresta(Animais) :- findall(Animal, animal(Animal, _, _, floresta, _), Animais).
+% 4. Quais são todos os animais que vivem na floresta?
+% animais_vivem_floresta(Animal).
 
 % 5. Quais os tipos de dietas dos animais que vivem na savana?
-% dietas_na_savana(Dietas).
-dietas_na_savana(Dietas) :- findall(Dieta, (animal(_, _, Dieta, savana, _)), DietasList), list_to_set(DietasList, Dietas).
+% dietas_animais_savana(Animal, Dieta)
 
 % 6. Existe algum animal ovíparo que vive em água salgada?
-% oviparo_agua_salgada(Existe).
-oviparo_agua_salgada(Existe) :- animal(_, _, _, agua_marinha, oviparo), Existe = sim ; Existe = nao.
+% existe_oviparo_agua_salgada(Existe, Animal)
 
 % 7. Existe algum animal mamífero que é ovíparo?
-% mamifero_oviparo(Existe).
-mamifero_oviparo(Existe) :- animal(_, mamifero, _, _, oviparo), Existe = sim ; Existe = nao.
+% existe_mamifero_oviparo(Existe, Animal)
 
-% 8. Existe algum animal carnívoro que viva em água doce?
-% carnivoro_agua_doce(Existe).
-carnivoro_agua_doce(Existe) :- animal(_, _, carnivoro, agua_doce, _), Existe = sim ; Existe = nao.
+% 8. Existe algum animal carnívoro que vive em água doce?
+% existe_carnivoro_aguaDoce(Existe, Animal)
 
 % 9. Quais são os animais terrestres herbívoros?
-% herbivoros_terrestres(Animais).
-herbivoros_terrestres(Animais) :- findall(Animal, (animal(Animal, _, herbivoro, Habitat, _), Habitat \= agua_marinha, Habitat \= agua_doce), Animais).
+% animais_terrestres_herbivoros(Animal)
 
 % 10. Quais animais voam e comem carne?
-% voadores_carnivoros(Animais).
-voadores_carnivoros(Animais) :- findall(Animal, (animal(Animal, ave, carnivoro, _, _)), Animais).
-
-% Consultas como comentários
-
-% % 1. Qual é o habitat de um animal específico X?
-% habitat(leao, Habitat). % Deve retornar savana
-
-% % 2. O animal específico Y é um mamífero?
-% eh_mamifero(elefante). % Deve retornar true
-
-% % 3. Quais são todos os animais carnívoros?
-% carnivoros(Animais). % Deve retornar [leao, tubarao_martelo, crocodilo, salamandra, gaviao, pinguim, sapo, raposa]
-
-% % 4. Quais são todos os animais vivem na floresta?
-% vivem_na_floresta(Animais). % Deve retornar [tucano, salamandra, gaviao, urso, raposa]
-
-% % 5. Quais os tipos de dietas dos animais que vivem na savana?
-% dietas_na_savana(Dietas). % Deve retornar [carnivoro, herbivoro]
-
-% % 6. Existe algum animal ovíparo que vive em água salgada?
-% oviparo_agua_salgada(Existe). % Deve retornar sim
-
-% % 7. Existe algum animal mamífero que é ovíparo?
-% mamifero_oviparo(Existe). % Deve retornar sim
-
-% % 8. Existe algum animal carnívoro que viva em água doce?
-% carnivoro_agua_doce(Existe). % Deve retornar sim
-
-% % 9. Quais são os animais terrestres herbívoros?
-% herbivoros_terrestres(Animais). % Deve retornar [elefante, girafa, tartaruga_marinha, pato]
-
-% % 10. Quais animais voam e comem carne?
-% voadores_carnivoros(Animais). % Deve retornar [gaviao, pinguim]
+% voadores_carnivoros(Animal)
